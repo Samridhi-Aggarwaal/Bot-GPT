a48292e8fbc1d179ed694823fd15076e6a3dce61
Samridhi <samridhi.agarwal001122@gmail.com>
2025-11-29 18:57:37 +0530

    Create database.py

---
+++ app/db/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db import models
+
+DATABASE_URL = "sqlite:///./bot_gpt.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    models.Base.metadata.create_all(bind=engine)
